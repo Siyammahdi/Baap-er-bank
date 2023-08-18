8aa0c3fac9da0a9db520674f0ccc057b759b1dc3
Siyammahdi <siyammahdi1@gmail.com>
2023-08-18 21:58:25 +0600

    baap er bank webiste done

--- deposit.js
+++ deposit.js
@@ -1,17 +1,27 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
-    const depositField = document.getElementById('deposit-field')
+    const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
-    const newDepositAmount = parseFloat(newDepositAmountString)
+    const newDepositAmount = parseFloat(newDepositAmountString);
     
     
-    const depositTotalElement = document.getElementById('deposit-total')
-    const previousDepositTotalString = depositTotalElement.innerText
-    const previousDepositTotal = parseFloat(previousDepositTotalString)
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);;
 
-    const currentDepositTotal = previousDepositTotal + newDepositAmount
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
 
-    depositTotalElement.innerText = currentDepositTotal
-
+    depositTotalElement.innerText = currentDepositTotal;
+    
     depositField.value = '';
+
+
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    balanceTotalElement.innerText = currentBalanceTotal;
     
 })
